e73a1e7571907e26eb280354b65a6aee153bad24
adibnoh <bromatrik@gmail.com>
2018-11-25 00:02:59 +0800

    phpcs fix

--- admin/addemployee.php
+++ admin/addemployee.php
@@ -2,55 +2,47 @@
 session_start();
 error_reporting(0);
 include('includes/config.php');
-if(strlen($_SESSION['alogin'])==0)
-    {   
-header('location:index.php');
-}
-else{
-if(isset($_POST['add']))
-{
-$empid=$_POST['empcode'];
-$fname=$_POST['firstName'];
-$lname=$_POST['lastName'];   
-$email=$_POST['email']; 
-$password=md5($_POST['password']); 
-$gender=$_POST['gender']; 
-$dob=$_POST['dob']; 
-$department=$_POST['department']; 
-$address=$_POST['address']; 
-$city=$_POST['city']; 
-$country=$_POST['country']; 
-$mobileno=$_POST['mobileno']; 
-$status=1;
+if (strlen($_SESSION['alogin'])==0) {
+    header('location:index.php');
+} else {
+    if (isset($_POST['add'])) {
+        $empid=$_POST['empcode'];
+        $fname=$_POST['firstName'];
+        $lname=$_POST['lastName'];
+        $email=$_POST['email'];
+        $password=md5($_POST['password']);
+        $gender=$_POST['gender'];
+        $dob=$_POST['dob'];
+        $department=$_POST['department'];
+        $address=$_POST['address'];
+        $city=$_POST['city'];
+        $country=$_POST['country'];
+        $mobileno=$_POST['mobileno'];
+        $status=1;
 
-$sql="INSERT INTO tblemployees(EmpId,FirstName,LastName,EmailId,Password,Gender,Dob,Department,Address,City,Country,Phonenumber,Status) VALUES(:empid,:fname,:lname,:email,:password,:gender,:dob,:department,:address,:city,:country,:mobileno,:status)";
-$query = $dbh->prepare($sql);
-$query->bindParam(':empid',$empid,PDO::PARAM_STR);
-$query->bindParam(':fname',$fname,PDO::PARAM_STR);
-$query->bindParam(':lname',$lname,PDO::PARAM_STR);
-$query->bindParam(':email',$email,PDO::PARAM_STR);
-$query->bindParam(':password',$password,PDO::PARAM_STR);
-$query->bindParam(':gender',$gender,PDO::PARAM_STR);
-$query->bindParam(':dob',$dob,PDO::PARAM_STR);
-$query->bindParam(':department',$department,PDO::PARAM_STR);
-$query->bindParam(':address',$address,PDO::PARAM_STR);
-$query->bindParam(':city',$city,PDO::PARAM_STR);
-$query->bindParam(':country',$country,PDO::PARAM_STR);
-$query->bindParam(':mobileno',$mobileno,PDO::PARAM_STR);
-$query->bindParam(':status',$status,PDO::PARAM_STR);
-$query->execute();
-$lastInsertId = $dbh->lastInsertId();
-if($lastInsertId)
-{
-$msg="Employee record added Successfully";
-}
-else 
-{
-$error="Something went wrong. Please try again";
-}
-
-}
-
+        $sql="INSERT INTO tblemployees(EmpId,FirstName,LastName,EmailId,Password,Gender,Dob,Department,Address,City,Country,Phonenumber,Status) VALUES(:empid,:fname,:lname,:email,:password,:gender,:dob,:department,:address,:city,:country,:mobileno,:status)";
+        $query = $dbh->prepare($sql);
+        $query->bindParam(':empid', $empid, PDO::PARAM_STR);
+        $query->bindParam(':fname', $fname, PDO::PARAM_STR);
+        $query->bindParam(':lname', $lname, PDO::PARAM_STR);
+        $query->bindParam(':email', $email, PDO::PARAM_STR);
+        $query->bindParam(':password', $password, PDO::PARAM_STR);
+        $query->bindParam(':gender', $gender, PDO::PARAM_STR);
+        $query->bindParam(':dob', $dob, PDO::PARAM_STR);
+        $query->bindParam(':department', $department, PDO::PARAM_STR);
+        $query->bindParam(':address', $address, PDO::PARAM_STR);
+        $query->bindParam(':city', $city, PDO::PARAM_STR);
+        $query->bindParam(':country', $country, PDO::PARAM_STR);
+        $query->bindParam(':mobileno', $mobileno, PDO::PARAM_STR);
+        $query->bindParam(':status', $status, PDO::PARAM_STR);
+        $query->execute();
+        $lastInsertId = $dbh->lastInsertId();
+        if ($lastInsertId) {
+            $msg="Employee record added Successfully";
+        } else {
+            $error="Something went wrong. Please try again";
+        }
+    }
     ?>
 
 <!DOCTYPE html>
